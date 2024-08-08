c44ab286b374a4b74dc5013e4192f08423c3ff1e
Aastha Agrawal <aasthabagrawal@gmail.com>
2024-08-08 12:05:42 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://Aastha:aastha11*@books-store-mern.8hdxv.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-store-MERN'
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express  from 'express';
+
+import { PORT,mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoute.js';
+
+const app = express();
+
+app.use(express.json());
+
+//Middleware for handling CORS POLICY
+app.use(cors());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+
+    })
+);
+app.get('/', (request,response) => {
+    console.log(request)
+    return response.status(234).send('Welcome to Book Store!!');
+});
+
+app.use('/books', booksRoute);
+
+mongoose
+.connect(mongoDBURL)
+.then(()=>{
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+        console.log(`App listening on port: ${PORT}`);
+    
+    });
+})
+.catch((error)=>{
+    console.log(error);
+});
+       
